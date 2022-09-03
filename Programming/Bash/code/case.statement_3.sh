74c3d0db0051b3d66d2ff22bc7a6370302e69503
aquecola <skwortsow.aleksander@yandex.ru>
2022-09-03 21:34:17 +0300

    Bash practice

---
+++ Programming/Bash/code/case.statement_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Do you like the bash course?" [yes/no]:
+read answer
+case $answer in
+	[Yy] | [Yy][Ee][Ss] )
+		echo "That's right!"
+		;;
+	[Nn] | [Nn][Oo] )
+		echo ":(";
+		exit 1
+		;;
+	*) echo "Enter Y[es] or N[o]"
+	 ;;
+esac
